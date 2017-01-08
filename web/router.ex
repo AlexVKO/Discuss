605b79de219ca9f27346b7640ca30d9434abb933
Alex VKO <ale@alexvko.com>
2017-01-08 20:13:47 -0200

    Adds hability to list topics

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Discuss.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/topics/new", TopicController, :new
+    get "/topics", TopicController, :index
     post "/topics", TopicController, :create
   end
 
