3fe1124e7d505d39d75bc1d4e1168af07e751c7a
Alex VKO <ale@alexvko.com>
2017-01-09 13:12:41 -0200

    Adds a plug for current user

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Discuss.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Discuss.Plugs.SetUser
   end
 
   pipeline :api do
@@ -24,9 +25,4 @@ defmodule Discuss.Router do
     get "/:provider", AuthController, :request
     get "/:provider/callback", AuthController, :callback
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", Discuss do
-  #   pipe_through :api
-  # end
 end
